bd28b48595a65e4d6da7b9e9b8f28eedcdc62f02
Slimane Deb <cs_deb@esi.dz>
2022-08-19 14:05:05 +0100

    Add files via upload

---
+++ list_pm.php
@@ -0,0 +1,70 @@
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link href="default/style.css" rel="stylesheet" title="Style">
+        <title>Messages Personnels</title>
+    </head>
+    <body>
+    	<div class="header">
+        	<img src="default/images/logo1.png" alt="Espace Membre">
+	    </div>
+        <div class="content">
+<div class="box">
+	<div class="box_left">
+    	<a href="index.php">Index du Forum</a>  Liste de vos Messages Personnels
+    </div>
+	<div class="box_right">
+    <span id="Nom"> User:</span>	<a href="profile.php?id=2">mm</a><a href="list_pm.php">Vos messages(0)</a><a href="login.php">Déconnexion</a></div>
+    <div class="clean"></div>
+</div>
+Voici la liste de vos messages:<br>
+<a href="new_pm.php" class="button">Nouveau message privé</a><br>
+<h3>Messages non-lus(0):</h3>
+<table class="list_pm">
+	<tbody><tr>
+    	<th class="title_cell">Titre</th>
+        <th>Nb. Réponses</th>
+        <th>Participant</th>
+        <th>Date d'envoi</th>
+    </tr>
+	<tr>
+    	<td colspan="4" class="center">Vous n'avez aucun message non-lu.</td>
+    </tr>
+</tbody></table>
+<br>
+<h3>Messages lus(4):</h3>
+<table class="list_pm">
+	<tbody><tr>
+    	<th class="title_cell">Titre</th>
+        <th>Nb. Réponses</th>
+        <th>Participant</th>
+        <th>Date d'envoi</th>
+    </tr>
+	<tr>
+    	<td class="left"><a href="read_pm.php?id=4">skmd</a></td>
+    	<td>0</td>
+    	<td><a href="profile.php?id=1">ff</a></td>
+    	<td>09/01/2015 18:17:22</td>
+    </tr>
+	<tr>
+    	<td class="left"><a href="read_pm.php?id=3">mm</a></td>
+    	<td>0</td>
+    	<td><a href="profile.php?id=1">ff</a></td>
+    	<td>06/01/2015 20:53:44</td>
+    </tr>
+	<tr>
+    	<td class="left"><a href="read_pm.php?id=2">ljnwxslk</a></td>
+    	<td>0</td>
+    	<td><a href="profile.php?id=1">ff</a></td>
+    	<td>06/01/2015 19:48:08</td>
+    </tr>
+	<tr>
+    	<td class="left"><a href="read_pm.php?id=1">ljnk</a></td>
+    	<td>0</td>
+    	<td><a href="profile.php?id=1">ff</a></td>
+    	<td>06/01/2015 18:16:19</td>
+    </tr>
+</tbody></table>
+		</div>
+		<div class="foot"><a href="http://www.supportduweb.com/scripts_tutoriaux-code-source-89-simple-php-forum-script-forum-en-php-facile-simple-script-code-telecharger-forum-php-gratuit-mysql.html">Decouvrer le monde</a><a href="http://www.supportduweb.com/">GeekAll</a></div>
+	
+</body></html>
